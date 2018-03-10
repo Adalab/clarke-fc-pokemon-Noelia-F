2935389018e8e1e74c3e80851006625077dda5f1
Noelia-F <noelia.fronton.g@gmail.com>
2018-03-10 18:23:49 +0100

    function to ordenate by ID and show types function

--- my-pokemon-project/src/components/App.js
+++ my-pokemon-project/src/components/App.js
@@ -12,11 +12,56 @@ class App extends Component{
 	}
 
 	componentDidMount(){
-		fetch('http://hp-api.herokuapp.com/api/characters')
-			.then(response => response.json())
-			.then(json => {
-				this.setState({ pokemons: json });
-			});
+		let listPokemons = this.state.pokemons;
+		listPokemons.push({name: 'bulbasur',id: 1,types: [{slot: 2,type: {name: 'poison'}},{
+slot: 1,type: {name: 'grass'}}],
+sprites: {front_default: 'https://vignette.wikia.nocookie.net/pokemon-super-fanon/images/4/43/Bulbasaur.png/revision/latest?cb=20151125013521&path-prefix=es'}}, {name: 'asdf',id: 10,types: [{
+slot: 1,type: {name: 'grass'}}],
+sprites: {front_default: 'https://vignette.wikia.nocookie.net/pokemon-super-fanon/images/4/43/Bulbasaur.png/revision/latest?cb=20151125013521&path-prefix=es'}}, {name: 'asfad',id: 7,types: [{slot: 2,type: {name: 'poison'}},{
+slot: 1,type: {name: 'grass'}}],
+sprites: {front_default: 'https://vignette.wikia.nocookie.net/pokemon-super-fanon/images/4/43/Bulbasaur.png/revision/latest?cb=20151125013521&path-prefix=es'}}, {name: 'dafasf',id: 3,types: [{slot: 2,type: {name: 'poison'}},{
+slot: 1,type: {name: 'grass'}}],
+sprites: {front_default: 'https://vignette.wikia.nocookie.net/pokemon-super-fanon/images/4/43/Bulbasaur.png/revision/latest?cb=20151125013521&path-prefix=es'}}, {name: 'rewhgfdg',id: 5,types: [{slot: 2,type: {name: 'poison'}},{
+slot: 1,type: {name: 'grass'}}],
+sprites: {front_default: 'https://vignette.wikia.nocookie.net/pokemon-super-fanon/images/4/43/Bulbasaur.png/revision/latest?cb=20151125013521&path-prefix=es'}}, {name: 'kjhgkgkgh',id: 2,types: [{slot: 2,type: {name: 'poison'}},{
+slot: 1,type: {name: 'grass'}}],
+sprites: {front_default: 'https://vignette.wikia.nocookie.net/pokemon-super-fanon/images/4/43/Bulbasaur.png/revision/latest?cb=20151125013521&path-prefix=es'}}, {name: 'trettwtgrwe',id: 9,types: [{slot: 2,type: {name: 'poison'}},{
+slot: 1,type: {name: 'grass'}}],
+sprites: {front_default: 'https://vignette.wikia.nocookie.net/pokemon-super-fanon/images/4/43/Bulbasaur.png/revision/latest?cb=20151125013521&path-prefix=es'}}, {name: 'sfdgdsgds',id: 11,types: [{slot: 2,type: {name: 'poison'}},{
+slot: 1,type: {name: 'grass'}}],
+sprites: {front_default: 'https://vignette.wikia.nocookie.net/pokemon-super-fanon/images/4/43/Bulbasaur.png/revision/latest?cb=20151125013521&path-prefix=es'}}, {name: 'sfgdsgd',id: 8,types: [{slot: 2,type: {name: 'poison'}},{
+slot: 1,type: {name: 'grass'}}],
+sprites: {front_default: 'https://vignette.wikia.nocookie.net/pokemon-super-fanon/images/4/43/Bulbasaur.png/revision/latest?cb=20151125013521&path-prefix=es'}}, {name: 'bulbasur',id: 14,types: [{slot: 2,type: {name: 'poison'}},{
+slot: 1,type: {name: 'grass'}}],
+sprites: {front_default: 'https://vignette.wikia.nocookie.net/pokemon-super-fanon/images/4/43/Bulbasaur.png/revision/latest?cb=20151125013521&path-prefix=es'}}, {name: 'sfdg',id: 12,types: [{slot: 2,type: {name: 'poison'}},{
+slot: 1,type: {name: 'grass'}}],
+sprites: {front_default: 'https://vignette.wikia.nocookie.net/pokemon-super-fanon/images/4/43/Bulbasaur.png/revision/latest?cb=20151125013521&path-prefix=es'}}, {name: 'bulbafsdssur',id: 13,types: [{slot: 2,type: {name: 'poison'}},{
+slot: 1,type: {name: 'grass'}}],
+sprites: {front_default: 'https://vignette.wikia.nocookie.net/pokemon-super-fanon/images/4/43/Bulbasaur.png/revision/latest?cb=20151125013521&path-prefix=es'}}, {name: 'fgsdfqwewr',id: 16,types: [{slot: 2,type: {name: 'poison'}},{
+slot: 1,type: {name: 'grass'}}],
+sprites: {front_default: 'https://vignette.wikia.nocookie.net/pokemon-super-fanon/images/4/43/Bulbasaur.png/revision/latest?cb=20151125013521&path-prefix=es'}}, {name: 'bulbasurtewtgtrr',id: 15,types: [{slot: 2,type: {name: 'poison'}},{
+slot: 1,type: {name: 'grass'}}],
+sprites: {front_default: 'https://vignette.wikia.nocookie.net/pokemon-super-fanon/images/4/43/Bulbasaur.png/revision/latest?cb=20151125013521&path-prefix=es'}});
+listPokemons.sort(function (a, b){
+  if (a.id > b.id) {
+    return 1;
+  }
+  if (a.id < b.id) {
+    return -1;
+  }
+  // a must be equal to b
+  return 0;
+});
+		this.setState({
+			pokemons: listPokemons
+		});
+
+
+		// fetch('http://hp-api.herokuapp.com/api/characters')
+		// 	.then(response => response.json())
+		// 	.then(json => {
+		// 		this.setState({ pokemons: json });
+		// 	});
 	}
 
 	handleChange(event){
@@ -34,7 +79,7 @@ class App extends Component{
 			console.log(this.state.pokemons)
 		return(
 				pokemonsToShow.map((pokemon, i) =>
-								<Pokemons key={i} name={pokemon.name} image={pokemon.image} />)
+								<Pokemons key={i} id={pokemon.id} name={pokemon.name} type={pokemon.types} image={pokemon.sprites.front_default} />)
 		);
 	}
 	render(){
--- my-pokemon-project/src/components/Pokemons.js
+++ my-pokemon-project/src/components/Pokemons.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 
 class Pokemons extends Component{
+	constructor(props){
+		super(props);
+		this.addType = this.addType.bind(this);
+		this.state = {
+			pokemonType: this.props.type
+		};
+	}
+	addType(){
+		let type = '';
+		for (let i = 0; i< this.state.pokemonType.length; i++){
+			type = type + ' ' + this.state.pokemonType[i].type.name;
+		}
+		return(
+			<p className="ch-box">
+				{type}
+			</p>
+		)
+	}
 	render(){
 		const styleImage = {
 			backgroundImage: `url(${this.props.image})`
@@ -10,18 +28,13 @@ class Pokemons extends Component{
 					<div className="image" style = {styleImage}>
 						<img height="0" width="0" src ={this.props.image} alt="Pokemons"></img>
             <div className="image__id">
-              <p className="id">ID/1</p>
+              <p className="id">ID/ {this.props.id}</p>
             </div>
 					</div>
           <div className="info">
             <h2 className="name">{this.props.name}</h2>
             <div className="characteristics">
-              <div className="ch-box">
-              <p>GRASS</p>
-              </div>
-              <div className="ch-box">
-              <p>FIRE</p>
-              </div>
+              <p>{this.addType()}</p>
             </div>
           </div>
 			</li>
